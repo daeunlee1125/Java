925aba4aeb8f47a00445c0d38f9b14682f93b96d
daeunlee1125 <daeunlee1125@gmail.com>
2025-07-21 17:50:30 +0900

    백준 7단계 문제3

--- CodingTest/src/step7/Main3.java
+++ CodingTest/src/step7/Main3.java
@@ -6,20 +6,28 @@ public class Main3 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		char[][] five = new char[5][15];
-		Arrays.fill(five, null);
+		
 		
 		for (int i=0; i<5; i++) {
-			five[i] = sc.next().toCharArray();
-		}
-		
-		String res =  "";
-		
-		for (int i = 0; i<15; i++) {
-			for (int j=0; j<5; j++) {
-				res += five[j][i];
+			char[] input = sc.next().toCharArray();
+			Arrays.fill(five[i], ' ');
+			for (int j=0; j<input.length; j++) {
+				five[i][j] = input[j];
 			}
 		}
 		
+		String res =  "";
+		try {
+			for (int i = 0; i<15; i++) {
+				for (int j=0; j<5; j++) {
+					if (five[j][i]!=' ') {
+						System.out.print(five[j][i]);
+					}
+					
+				}
+			}
+		} catch(Exception e) {
+		}
 		System.out.println(res);
 		sc.close();
 	}
